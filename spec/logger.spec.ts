d6a4409f462b198225ce71876e5001969a1cf8df
devsheva <sheshi.mateo01@gmail.com>
2024-09-03 00:27:30 +0200

    feat(logger): add name and level property

---
+++ spec/logger.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from 'vitest'
+import Base from '../src/base'
+import { loggerDecorator } from '../src/logger'
+
+describe('logger', () => {
+  it('has a logger property', () => {
+    class Test extends Base {}
+    loggerDecorator()(Test)
+
+    expect(Test.prototype).toHaveProperty('logger')
+  })
+
+  it('overrides name if not provided', () => {
+    class Test extends Base {}
+    loggerDecorator()(Test)
+  })
+
+  describe('options', () => {
+    describe('level', () => {
+      it('defaults to info', () => {
+        class Test extends Base {}
+        loggerDecorator()(Test)
+        const instance = new Test()
+        expect(instance.logger).toHaveProperty('level', 'info')
+      })
+
+      it('can be overridden', () => {
+        class Test extends Base {}
+        loggerDecorator('', { level: 'debug' })(Test)
+        const instance = new Test()
+        expect(instance.logger).toHaveProperty('level', 'debug')
+      })
+    })
+  })
+})
---
+++ src/logger.ts
@@ -0,0 +1,30 @@
+type LogLevel = 'verbose' | 'debug' | 'info' | 'warn' | 'error'
+
+type LoggerOptions = {
+  level?: LogLevel
+}
+
+function Logger(
+  name?: string,
+  options: LoggerOptions = {
+    level: 'info'
+  }
+): ClassDecorator {
+  return function (target: Function) {
+    name = name ?? target.name
+
+    const logger = {
+      name,
+      level: options.level
+    }
+
+    Object.defineProperty(target.prototype, 'logger', {
+      value: logger
+    })
+  }
+}
+
+const loggerDecorator = Logger
+
+export default Logger
+export { LogLevel, loggerDecorator }
